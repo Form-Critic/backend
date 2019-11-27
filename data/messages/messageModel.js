3644f25a758465f1498f12490d242fe5eb0d3f67
Uzias Rivera <uziasrivera@gmail.com>
2019-11-26 17:17:50 -0800

    finishing messenger feature

--- data/messages/messageModel.js
+++ data/messages/messageModel.js
@@ -3,29 +3,26 @@ const db = require('../dbConfig')
 module.exports = {
     send,
     getConversation,
-    getByUser
+    getByUser,
+    sendReply,
+    getUsersConversations
 }
 
 async function send(id, body){
     //when creating a new message
     //user_one should always be assigned to the currently logged in user
     const conversationExists = await getConversation(id.user_one, id.user_two)
-    console.log('is this defined', !conversationExists[0])
+    console.log(conversationExists)
     if (!conversationExists[0]){
-        console.log('this is read')
     const message_id = await db('messages').insert({user_one:id.user_one, user_two:id.user_two}).select('*').returning('*')
-    console.log('this needs to hit', message_id)
-    const reply_id = await sendReply({...body, message_id: message_id[0], user_id: id.user_one})
-    return getById(reply[0])
+    return sendReply({...body, message_id: message_id[0], user_id: id.user_one}) 
 }else{
-    const reply_id = await sendReply({...body, message_id: conversationExists[0].id, user_id: id.user_one})
-    console.log(reply_id)
-    return getById(reply_id[0])
+    return sendReply({...body, message_id: conversationExists[0].id, user_id: id.user_one})
 }
 }
 
 function getConversation(user_id, other_id){
-    // for conversation thread\
+    // for conversation thread\ --> checks to see if a conversation exists between current user and another
     return db('messages').where({user_one: user_id}).andWhere({user_two:other_id}).orWhere({user_one: other_id}).andWhere({user_two:user_id}).select('*').returning('*')
 }
 
@@ -39,7 +36,22 @@ function getById(id){
     return db('message_conversation').where({id:id})
 }
 
-function sendReply(body){
+async function sendReply(body){
     //body must include post_id, user_id, text
-    return db('message_conversation').insert(body).select('*').returning('*')
+    reply_id = await db('message_conversation').insert(body).select('*').returning('*')
+    return getById(reply_id[0])
 }
+
+function getUsersConversations(){
+    return db('messages as m')
+    .join('users as u', 'm.user_id', 'u.id')
+    .where({user_one: user_id}).orWhere({user_two: other_id}).select('m.*, u.name').returning('m.*, u.name')
+
+}
+
+
+// async function getFullConversation(id){
+//     //id belongs to user_two
+//     constgetConversation(user_id, other_id)
+
+// }
--- data/messages/messageRouter.js
+++ data/messages/messageRouter.js
@@ -2,14 +2,15 @@ const messageDB = require('./messageModel')
 const router = require('express').Router()
 const restricted = require('../auth/restricted-middleware')
 
-router.post('/:id', restricted, (req,res)=>{
-    //where id is the receiver
+router.post('/user/:id', restricted, (req,res)=>{
+    //where id is the user_two
     const messageUsers = {user_one:req.decodedJwt.sub, user_two: req.params.id}
     const body = {...req.body}
     messageDB.send(messageUsers,body)
-    .then(count=>{
-        console.log(count)
-        res.status(200).json(count)
+    .then(message=>{
+        //upon success will create a record of a conversation and add a message
+        console.log(message)
+        res.status(200).json(...message)
     })
     .catch(err=>{
         console.log(err)
@@ -18,11 +19,25 @@ router.post('/:id', restricted, (req,res)=>{
 
 })
 
-// router.get('/', restricted, (req,res)=>[
+router.post('/:message_id', restricted, (req,res)=>{
+    //will be used for replies!!!!! id => message id 
+    const body = {...req.params, user_id:req.decodedJwt.sub, ...req.body}
+    messageDB.sendReply(body)
+    .then(reply=>{
+        res.status(201).json(...reply)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json(err)
+    })
 
-// ])
+})
 
-//middleware
+router.get('/', restricted, (req,res)=>[
+
+])
+
+//middleware may not be necessary 
 async function conversationExists(req,res,next){
     const messageUsers = {user_one:req.decodedJwt.sub, user_two: req.params.id}
     const conversation = await messageDB.getConversation(messageUsers.user_one, messageUsers.user_two)
